c7d2c288feb2d4a2a51d9f1083881d0583164a70
Wu Cong <370368132@qq.com>
2018-03-29 17:24:06 +0800

    11

---
+++ src/DataStructure/TreeLinkNode.java
@@ -0,0 +1,12 @@
+package DataStructure;
+
+
+// Definition for binary tree with next pointer.
+ public class TreeLinkNode {
+    public int val;
+    public TreeLinkNode left;
+	public TreeLinkNode right;
+	public TreeLinkNode next;
+    public TreeLinkNode(int x) { val = x; }
+ }
+ 
