b3f7749de87ad9b41fd1591e7b9037f1cc84a026
dell <dell@DESKTOP-KAOFQ8E>
2018-04-08 01:02:57 +0800

    1

--- src/leetCode100/P139WordBreak.java
+++ src/leetCode100/P139WordBreak.java
@@ -2,9 +2,12 @@ package leetCode100;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class P139WordBreak {
+	//Time limit Exceeded
     static public boolean wordBr1eak(String s, List<String> wordDict) {
     	boolean ret = false;
     	if(s.length()==0)
@@ -18,13 +21,40 @@ public class P139WordBreak {
     }
     
     static public boolean wordBreak(String s, List<String> wordDict){
-    
-    	for(String word :wordDict){
-    		s.startsWith(word)
-    	}
     	
-    	return false;
+    	boolean[] dj = new boolean[s.length()+1];
+    	
+    	Collections.sort(wordDict,new Comparator<String>(){
+
+
+			@Override
+			public int compare(String o1, String o2) {
+				// TODO Auto-generated method stub
+				
+				return o1.length()-o2.length();
+			}});
+    	
+    	for(int i = 0;i<=wordDict.size();i++){
+    		dj[0] = true;
+    	}
+    	for(int i=1;i<=s.length();i++){
+    		for(int j=1;j<=wordDict.size();j++){
+    			String word = wordDict.get(j-1);
+    			int wordl = word.length();
+    			if(i>=wordl&&match(s,i-wordl,word)){
+    				dj[i]=dj[i]||dj[i-wordl];
+    			}
+    		}
+    	}
+    	return dj[s.length()];
     }
+    static public boolean match(String s,int start,String b){
+    	for(int i=0;i<b.length();i++){
+    		if(s.charAt(i+start)!=b.charAt(i))return false;
+    	}
+    	return true;
+    }
+    
     public static void main(String[] args) {
     	String s = "dogs";
     	ArrayList<String> as = new ArrayList<>(Arrays.asList("dog","s","gs"));
