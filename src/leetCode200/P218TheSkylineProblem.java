4d652caef08626784e91590719a4a5910f9eb82f
Wu Cong <370368132@qq.com>
2018-07-06 21:36:44 +0800

    1

---
+++ src/leetCode200/P218TheSkylineProblem.java
@@ -0,0 +1,33 @@
+package leetCode200;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class P218TheSkylineProblem {
+	//这道题，我第一眼就感觉要用扫描线来解
+	//从左往右扫描//这是很蠢的一点，我压根就不懂线段树，所以也无法用线段树来求解这个问题。
+	//只有另辟蹊径
+
+    public List<int[]> getSkyline(int[][] buildings) {
+
+    	
+    	
+    	return null;
+    }
+    
+	public static void main(String[] args) {
+		
+	}
+	
+	private class BiNode{
+		int[] value;
+		BiNode left;
+		BiNode right;
+		public BiNode(int[] value) {
+			super();
+			this.value = value;
+		}
+	}
+}
+
+
