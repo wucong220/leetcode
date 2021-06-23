814a1ebe37cbe1f7a2465e10aaf75c177fbca331
wucong220 <370368132@qq.com>
2021-06-24 00:22:36 +0800

    1

---
+++ src/main/java/com/wu/CN149.java
@@ -0,0 +1,21 @@
+package com.wu;
+
+public class CN149 {
+    class Solution {
+        public int maxPoints(int[][] points) {
+            int max = Integer.MIN_VALUE;
+            for(int i=0;i<points.length;i++){
+                for(int j=i+1;j<points.length;j++){
+                    int ctr = 2;
+                    for(int k=j+1;k<points.length;k++){
+                        if((points[i][0]-points[j][0])*(points[i][1]-points[k][1])==(points[i][1]-points[j][1])*(points[i][0]-points[k][0])){
+                            ctr++;
+                        }
+                    }
+                    max = Math.max(ctr,max);
+                }
+            }
+            return max;
+        }
+    }
+}
